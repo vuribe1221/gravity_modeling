6e4c1c155d7385491a47e7dbb59d5dd674ae0c62
vuribe <vuribe1221@gmail.com>
2013-10-22 00:13:10 -0700

    add file

---
+++ addFiles.sh
@@ -0,0 +1,30 @@
+#Begin------Contents of gravmodeling.sh
+#!/bin/bash
+
+if [ -f ./sum ]
+then
+	rm sum
+fi
+
+
+cut -f 3 -d ' ' *.out > col
+
+split -dl 3 col col.
+
+paste -d"+" col.*|while read line
+	do
+	#echo -n"$line="
+	echo $line|bc >> sum
+	done
+
+rm col*
+
+let i=1
+let j=`wc -l sum|cut -f1 -d ' '`
+
+while [ $i -le $j ];
+do
+	echo $i' '1 `head -n $i sum|tail -n 1` 
+	let i=$i+1
+done
+#End--------Contents of chunkFile.sh
